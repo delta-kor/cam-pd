7f75b68405e012c5307aa08dfe7cae2a979d30db
delta-kor <deltakbot1@gmail.com>
2021-09-11 21:18:28 +0900

    Add stage creation

--- cam-pd-server/src/controllers/stage.controller.ts
+++ cam-pd-server/src/controllers/stage.controller.ts
@@ -1,9 +1,35 @@
+import CreateStageDto from '../dtos/create-stage.dto';
 import StageService from '../services/stage.service';
+
+namespace StageResponse {
+  export interface Create extends ApiResponse {
+    uuid: string;
+    title: string;
+    concert: string;
+    video_id: string;
+  }
+}
 
 class StageControllerClass {
   private readonly stageService = StageService;
 
-  public async createStage(req: TypedRequest, res: TypedResponse): Promise<void> {}
+  public async createStage(
+    req: TypedRequest<CreateStageDto>,
+    res: TypedResponse<StageResponse.Create>
+  ): Promise<void> {
+    const stage = await this.stageService.create(
+      req.body.title,
+      req.body.concert,
+      req.body.video_id
+    );
+    res.json({
+      ok: true,
+      uuid: stage.uuid,
+      title: stage.title,
+      concert: stage.concert,
+      video_id: stage.videoId,
+    });
+  }
 }
 
 const StageController = new StageControllerClass();
