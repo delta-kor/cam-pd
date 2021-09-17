a83147ec5fb98389dcf86cf05c1639237a98d995
delta-kor <deltakbot1@gmail.com>
2021-09-17 17:33:44 +0900

    Fix route type

--- cam-pd-server/src/types/express.d.ts
+++ cam-pd-server/src/types/express.d.ts
@@ -9,7 +9,7 @@ declare namespace Express {
 type TypedRequest<P = any, Q = any> = import('express').Request<any, any, P, Q>;
 type TypedResponse<P extends ApiResponse = ApiResponse> = import('express').Response<P>;
 type Route = (
-  req: import('express').Request,
-  res: import('express').Response,
+  req: TypedRequest,
+  res: TypedResponse,
   next: import('express').NextFunction
 ) => Promise<any>;
