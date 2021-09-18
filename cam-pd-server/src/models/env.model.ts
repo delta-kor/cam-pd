122cdc8858421892f020603c8caeeb01f9cf817a
delta-kor <deltakbot1@gmail.com>
2021-09-18 11:56:53 +0900

    Add env model

---
+++ cam-pd-server/src/models/env.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+
+export interface Env extends Document {
+  key: string;
+  value: string;
+}
+
+const EnvSchema = new Schema<Env>({
+  key: { type: String, required: true, unique: true },
+  value: { type: String, required: true },
+});
+
+const EnvModel = model<Env>('Env', EnvSchema);
+
+export default EnvModel;
