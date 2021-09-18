2a4966a3c8bddce460d16a9bfcbd6c45b067c150
delta-kor <deltakbot1@gmail.com>
2021-09-18 12:14:32 +0900

    Add vimeo data fetcher

--- cam-pd-server/src/utils/vimeo.util.ts
+++ cam-pd-server/src/utils/vimeo.util.ts
@@ -1,4 +1,12 @@
+import axios from 'axios';
 import EnvModel from '../models/env.model';
+
+interface VimeoVideoDataItem {
+  quality: string;
+  type: string;
+  link: string;
+  size: number;
+}
 
 class VimeoUtilClass {
   private jwt: string | null = null;
@@ -10,9 +18,38 @@ class VimeoUtilClass {
     this.jwt = jwt.value;
   }
 
+  private async fetchVideoData(videoId: string): Promise<string> {
+    if (!this.jwt) throw new Error('Jwt is not loaded');
+
+    const url = `https://api.vimeo.com/videos/${videoId}?fields=files.size,files.link,files.type,files.quality`;
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `jwt ${this.jwt}`,
+      },
+    });
+
+    const data = response.data;
+    if (data.error) throw new Error('Jwt expired');
+
+    const files: VimeoVideoDataItem[] = data.files;
+    if (!files) throw new Error('Vimeo video data not found');
+
+    let max: number = 0;
+    for (const file of files) {
+      if (file.type !== 'video/mp4') continue;
+      max = Math.max(file.size, max);
+    }
+
+    for (const file of files) {
+      if (file.size === max) return file.link;
+    }
+
+    return files[0].link;
+  }
+
   public async getVideoUrl(videoId: string): Promise<string> {
     await this.loadJwt();
-    return 'https://google.com/';
+    return await this.fetchVideoData(videoId);
   }
 }
 
