36790d3436a0f5392b6eb082b267d415c62c9bc9
delta-kor <deltakbot1@gmail.com>
2021-09-19 20:16:20 +0900

    Add selector tag

--- cam-pd-client/src/components/actions/CameraSelector.tsx
+++ cam-pd-client/src/components/actions/CameraSelector.tsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import styled from 'styled-components';
 import Config from '../../config';
 import Talker from '../../services/talker';
+import { Colors } from '../../styles';
 
 const Layout = styled.div`
   position: relative;
@@ -10,9 +11,38 @@ const Layout = styled.div`
 `;
 
 const Video = styled.video`
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
+`;
+
+const SelectorWrapper = styled.div`
+  position: absolute;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-content: start;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const SelectorItem = styled.div`
+  position: relative;
+  height: calc(50vw / 2 * (9 / 16));
+`;
+
+const SelectorTitle = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 4px 16px;
+  font-weight: normal;
+  font-size: 12px;
+  color: ${Colors.WHITE};
+  background: ${Colors.BLACK};
+  user-select: none;
 `;
 
 interface Props {
@@ -30,9 +60,20 @@ class CameraSelector extends Component<Props, State> {
     // prettier-ignore
     const url = `${Config.base_url}/stage/video?ticket=${encodeURI(ticket)}&token=${encodeURI(token)}&uuid=${uuid}`;
 
+    const selectorItems = [];
+
+    for (let i = 0; i < 6; i++) {
+      selectorItems.push(
+        <SelectorItem key={i}>
+          <SelectorTitle>CAM{i + 1}</SelectorTitle>
+        </SelectorItem>
+      );
+    }
+
     return (
       <Layout>
         <Video src={url} />
+        <SelectorWrapper>{selectorItems}</SelectorWrapper>
       </Layout>
     );
   }
